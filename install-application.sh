a97212e1ac024bc514d32237b9b3b17d19a756df
secrectvn <quoclong.hacker1412@gmail.com>
2020-05-04 14:47:15 +0700

    Update install-application.sh

--- install-application.sh
+++ install-application.sh
@@ -3,7 +3,7 @@
 sudo apt install git curl unzip htop flameshot curl -y 
 
 ### Install Brave
-sudo apt install apt-transport-https curl && \
+sudo apt install apt-transport-https -y && \
 curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add - && \
 echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list && \
 sudo apt update && \
